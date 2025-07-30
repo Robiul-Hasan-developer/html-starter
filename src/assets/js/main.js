51402fca033988b60d513d76329770abd04a063e
Robiul Hasan <robiulhasan9559@gmail.com>
2025-07-30 11:40:51 +0600

    (Added) Some code added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -286,28 +286,37 @@
     // // ================================= Brand slider End =========================
 
     // ========================= Counter Up Js End ===================
-    //  const counterUp = window.counterUp.default;
+    // const counterUp = window.counterUp.default;
 
-    //  const callback = (entries) => {
-    //    entries.forEach((entry) => {
-    //      const el = entry.target;
-    //      if (entry.isIntersecting && !el.classList.contains('is-visible')) {
-    //        counterUp(el, {
-    //          duration: 2000,
-    //          delay: 16,
-    //        });
-    //        el.classList.add('is-visible');
-    //      }
-    //    });
-    //  };
+    // const callback = (entries) => {
+    //   entries.forEach((entry) => {
+    //     const el = entry.target;
+    //     if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+    //       counterUp(el, {
+    //         duration: 1500,
+    //         delay: 16,
+    //       });
+    //       el.classList.add("is-visible");
+    //     }
+    //   });
+    // };
+    // const IO = new IntersectionObserver(callback, { threshold: 1 });
 
-    //  const IO = new IntersectionObserver(callback, { threshold: 1 });
+    // // Banner statistics Counter
+    // const statisticsCounter = document.querySelectorAll(".counter");
+    // if (statisticsCounter.length > 0) {
+    //   statisticsCounter.forEach((counterNumber) => {
+    //     IO.observe(counterNumber);
+    //   });
+    // }
 
-    //  // Counter
-    //  const counter = document.querySelector('.counter');
-    //  if (counter) {
-    //    IO.observe(counter);
-    //  }
+    // // performance Count
+    // const performanceCount = document.querySelectorAll(".counter");
+    // if (performanceCount.length > 0) {
+    //   performanceCount.forEach((counterNumber) => {
+    //     IO.observe(counterNumber);
+    //   });
+    // }
     // ========================= Counter Up Js End ===================
 
     // ========================== Add Attribute For Bg Image Js Start ====================
